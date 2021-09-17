016decba8f4498542e0655a34a98aeeacb9f4f64
Douglas Alexandre dos Santos <42381155+Douglas08Santos@users.noreply.github.com>
2021-09-16 22:59:04 -0300

    Update ProcessoAgil.py
    Para entrega

--- ProcessoAgil.py
+++ ProcessoAgil.py
@@ -1,22 +1,75 @@
 import requests, re, json
 
-try:
-	req = requests.get('http://g1.globo.com')
-	#print(req.text)
+# execucao: python3 ProcessoAgil.py
+
+#bibliotecas necessarias:
+
+# - requests
+
+ # - re
+
+ # - json
+
 	
+
+'''
+
+Utilizando try-expect para capturar de exceções caso 
+
+não seja possivel se obter resposta da requisição
+
+'''
+
+try:
+
+	#usando o get requests e obtendo como resposta uma string com o codigo(ex HTML) da pagina
+
+	req = requests.get('http://g1.globo.com')
+
+	
+
+	'''
+
+	Analisando o codigo da pagina foi possivel notar que o titulo(title) e
+
+	o subtitle(summary) aparece na seguinte ordem:
+
+		
+
+		..."summary":"Texto do Subtitulo","title":"texto do subtitulo"
+
+	
+
+	assim criei essa expressão regular que captura somente o conteudo(se houver, por isso o ?)dos pares subtitulo e do titulo.
+
+	'''
+
 	regex = r'"summary":(".*?"),"title":(".*?")'
 
+	# procura todas as substrings que se encaixa no regex que criei
+
 	result = re.findall(regex, req.text)
-	#print(result)
-	#print()
-	#print(result[1])
+
 	
-	#print(result[1][0])
-	#print(result[1][1])
+
+	'''
+
+	Utilizando Compreensão de lista crio uma lista de dicionarios para que seja mais facil impressão em Json
+
 	
-	result_json = [{"titulo": result[i][1], "subtitulo":result[i][0]} for i in range(len(result))
+
+	'''
+
+	result_json = [{"titulo": result[i][1], "subtitulo":result[i][0]} for i in range(len(result))]
+
 	
+
+	#Impressão no terminal o Json obtido
+
+	
+
 	print(json.dumps(result_json, indent = 4,ensure_ascii=False))
 
 except Exception as e:
+
 	print('Erro: ' , e)
